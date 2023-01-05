2088786d99f967bb5b606176989dbfec44a9248c
Vladimir-Tkachev <Tkachev1008@mail.ru>
2023-01-05 20:44:37 +0300

    feat:add humanNumber

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function toReadable (number) {
-    let ones = [
+     let ones = [
         "",
         "one",
         "two",
@@ -34,26 +34,26 @@ module.exports = function toReadable (number) {
         "ninety",
     ];
 
-    let numString = number.toString();
+let numString = number.toString();
 
-    if (number < 0) {
-        return Math.abs(number);
-    
+if(number < 0) {
+    // return Math.abs(number)
+console.log('Negative number')
+}
+ if(number === 0) {
+    return 'zero'
+ }if (number < 20) {
+    return ones[number];
+}
+if (numString.length === 2){
+    return (tens[numString[0]] + ' ' + ones[numString[1]]);
+    // return tens[numString[0]]
+}
+if(numString.length === 3){
+    if(numString[1] === '' && numString[2]=== '0'){
+        return ones[numString[0]] + ' hundred'
+    }else {
+        return ones[numString[0]] + " hundred " + (tens[numString[1]] + ones[numString[2]]);
     }
-    if (number === 0) {
-        return "zero";
-    }
-    if (numString.length === 2) {
-        return tens[numString[0]] + " " + ones[numString[1]];
-        // return tens[numString[0]]
-    }
-    if (numString.length === 3) {
-        return (
-            ones[numString[0]] +
-            " hundred " +
-            tens[numString[1]] +
-            " " +
-            ones[numString[2]]
-        );
-    }
+// console.log(ones[numString[0]] + ' Hundred ' +( ones[number]))
 }
