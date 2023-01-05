aa0bf7aa960f2d8684542fb506e1bf947c38d1cb
Vladimir-Tkachev <Tkachev1008@mail.ru>
2023-01-05 20:42:19 +0300

    feat:add humanNumber

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 module.exports = function toReadable (number) {
-    var th = ["", "thousand", "million", "billion", "trillion"];
-    var dg = [
-        "zero",
+    let ones = [
+        "",
         "one",
         "two",
         "three",
@@ -11,8 +10,6 @@ module.exports = function toReadable (number) {
         "seven",
         "eight",
         "nine",
-    ];
-    var tn = [
         "ten",
         "eleven",
         "twelve",
@@ -24,7 +21,9 @@ module.exports = function toReadable (number) {
         "eighteen",
         "nineteen",
     ];
-    var tw = [
+    let tens = [
+        "",
+        "",
         "twenty",
         "thirty",
         "forty",
@@ -35,41 +34,26 @@ module.exports = function toReadable (number) {
         "ninety",
     ];
 
-    number = number.toString();
-    number = number.replace(/[\, ]/g, "");
-    if (number != parseFloat(number)) return "not a number";
-    var x = number.indexOf(".");
-    if (x == -1) x = number.length;
-    if (x > 15) return "too big";
-    var n = number.split("");
-    var str = "";
-    var sk = 0;
-    for (var i = 0; i < x; i++) {
-        if ((x - i) % 3 == 2) {
-            if (n[i] == "1") {
-                str += tn[Number(n[i + 1])] + " ";
-                i++;
-                sk = 1;
-            } else if (n[i] != 0) {
-                str += tw[n[i] - 2] + " ";
-                sk = 1;
-            }
-        } else if (n[i] != 0) {
-            // 0235
-            str += dg[n[i]] + " ";
-            if ((x - i) % 3 == 0) str += "hundred ";
-            sk = 1;
-        } if ((x - i) % 3 == 1) {
-            if (sk) str += th[(x - i - 1) / 3] + " ";
-            sk = 0;
-        }
-      
-    }
+    let numString = number.toString();
 
-    if (x != number.length) {
-        var y = number.length;
-        str += "point ";
-        for (var i = x + 1; i < y; i++) str += dg[n[i]] + " ";
+    if (number < 0) {
+        return Math.abs(number);
+    
     }
-    return str.replace(/\s+/g, " ");
+    if (number === 0) {
+        return "zero";
+    }
+    if (numString.length === 2) {
+        return tens[numString[0]] + " " + ones[numString[1]];
+        // return tens[numString[0]]
+    }
+    if (numString.length === 3) {
+        return (
+            ones[numString[0]] +
+            " hundred " +
+            tens[numString[1]] +
+            " " +
+            ones[numString[2]]
+        );
+    }
 }
