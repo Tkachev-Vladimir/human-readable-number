6a67b23a6baa4279a94f23bdcd14a84614f79727
Vladimir-Tkachev <Tkachev1008@mail.ru>
2023-01-11 15:17:00 +0300

    feat:update js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
- module.exports = function toReadable(number) {
+ module.exports = 
+ function toReadable(number) {
 
 let ones = [
     " ",
@@ -13,8 +14,7 @@ let ones = [
     "nine",
 ];
 let tens = [
-    
-    "ten",
+   "ten",
     "eleven",
     "twelve",
     "thirteen",
@@ -41,21 +41,33 @@ let twelve = [
 
     let numString = number.toString();
 
-    if(numString == 0){
-        return  'zero';
-    }else if (numString <10){
+    if (numString == 0) {
+        return "zero";
+    } else if (numString < 10) {
         return ones[numString];
-    }else if( numString < 20){
+    } else if (numString >= 10 && numString < 20) {
         return tens[numString[1]];
-    }else if(numString<100) {
-        return twelve[numString[0]] + ' ' + ones[numString[1]]
-    }else if ( numString >100  && numString[1] != 1 && numString[1] != 0){
-        return ones[numString[0]] +' hundred ' + twelve[numString[1]] + ' ' + ones[numString[2]];
-    } else if (numString >100 && numString[1] == 1 && numString[2] != 0){
+    } else if (numString >= 20 && numString < 100) {
+        return twelve[numString[0]] + "" + ones[numString[1]];
+    }
+    //     else if(numString == 100){
+    //  return ones[numString[0]] + 'hundred'
+    //     }
+    else if (numString >= 100 && numString[1] != 1 && numString[1] != 0) {
+        return (
+            ones[numString[0]] +
+            " hundred " +
+            twelve[numString[1]] +
+            " " +
+            ones[numString[2]]
+        );
+    } else if (numString >= 100 && numString[1] == 1 && numString[2] != 0) {
         return ones[numString[0]] + " hundred " + tens[numString[2]];
-    }else if ( numString > 100 && numString[1]==0){
-        return ones[numString[0]] + ' hundred ' + ones[numString[2]]
-    }else {
-        return undefined
+    } else if (numString >= 100 && numString[1] == 1 && numString[2] != 0) {
+        return ones[numString[0]] + " hundred " + tens[numString[2]];
+    } else if (numString >= 100 && numString[1] == 0) {
+        return ones[numString[0]] + " hundred " + ones[numString[2]];
+    } else if (numString >= 100 && numString[1] == 1 && numString[2] == 0) {
+        return ones[numString[0]] + " hundred " + tens[numString[2]];
     }
  }
--- src/js.js
+++ src/js.js
@@ -63,7 +63,7 @@ else if (numString.length ===2){
 // }
 }
 
-console.log(toReadable(200));
+// console.log(toReadable(200));
 
 
 const single = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
@@ -75,33 +75,13 @@ function toReadable (number) {
   str = number.toString();
   if (str == 0){
     result = 'zero';
-    return result;
-  } else if (str < 10) {
-    result = `${single[str]}`;
-    return result.trim();
-  } else if (str >= 10 && str < 20) {
-    result = `${teens[str[1]]}`;
-    return result.trim();  
-  } else if (str >= 20 && str < 100) {
-    result = `${decades[str[0]]} ${single[str[1]]}`;
-    return result.trim();
+    return result;}
 
-
-
-  } else if (str >= 100 && str[1] != 1 && str[1] != 0){
-    result = `${single[str[0]]} ${hundred} ${decades[str[1]]} ${single[str[2]]}`;
-    return result.trim();
-
-
-
-
-    re
-  } else if (str >= 100 && str[1] == 1 && str[1] != 0) {
+ 
+  if (str >= 100 && str[1] == 1 && str[1] != 0) {
     result = `${single[str[0]]} ${hundred} ${teens[str[2]]}`;
     return result.trim();
-  } else if (str >= 100 && str[1] == 0) {
-    result = `${single[str[0]]} ${hundred} ${single[str[2]]}`;
-    return result.trim();    
-  } else {
-    return 'LoL';
-  }}
+  } 
+ 
+  }
+  console.log(toReadable(520))
